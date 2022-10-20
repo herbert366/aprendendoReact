ea93aaea2c4f163682fb2e2ca8c22f1cd70637d7
herbert366 <herbert.artur18@gmail.com>
2022-10-20 18:29:17 -0300

    terminado de deixar em ts

--- 01-github-explorer/src/components/RepositoryList.tsx
+++ 01-github-explorer/src/components/RepositoryList.tsx
@@ -2,8 +2,14 @@ import { RepositoryItem } from './RepositoryItem'
 import '../style/repositories.scss'
 import { useState, useEffect } from 'react'
 
+interface repository {
+  name: string
+  description: string
+  html_url: string
+}
+
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([])
+  const [repositories, setRepositories] = useState<repository[]>([])
 
   useEffect(() => {
     fetch('https://api.github.com/orgs/rocketseat/repos')
