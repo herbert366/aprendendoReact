7910aca7321efb6685732583b7cc735bf31b5669
herbert366 <herbert.artur18@gmail.com>
2022-10-27 22:17:04 -0300

    adicionando modal no botão de trans

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,12 +1,30 @@
+import { useState } from 'react'
 import { Dashboard } from './components/Dashboard/Dashboard'
 import { Header } from './components/Header'
 import { GlobalStyle } from './style/global'
+import Modal from 'react-modal'
+
+Modal.setAppElement('#root')
 
 export function App() {
+  const [isNewTransactionOpen, setIsNewTransactionOpen] = useState(false)
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionOpen(true)
+  }
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionOpen(false)
+  }
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+
+      <Modal
+        isOpen={isNewTransactionOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Nova transação meu bom :) </h2>
+      </Modal>
       <GlobalStyle />
     </>
   )
