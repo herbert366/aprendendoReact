0da6664f4a2f3b00628aba6445ff2912f2a3be33
herbert366 <herbert.artur18@gmail.com>
2022-10-27 21:50:38 -0300

    colocando api fake pra rodar

--- dtmoney/src/index.tsx
+++ dtmoney/src/index.tsx
@@ -1,6 +1,28 @@
+import { createServer } from 'miragejs'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
+
+createServer({
+  routes() {
+    this.namespace = 'api'
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'food',
+          createdAt: new Date(),
+        },
+      ]
+    })
+    this.namespace = ''
+    this.passthrough()
+  },
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
