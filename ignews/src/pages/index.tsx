07fb8991b60409d55aea2c9f432c290e2ca03933
herbert366 <herbert.artur18@gmail.com>
2022-11-14 23:40:22 -0300

    definindo estilo globais

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,9 +1,15 @@
 import styles from '../styles/Home.module.scss'
+import Head from 'next/head'
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>
-      Hello <span>come</span>
-    </h1>
+    <>
+      <Head>
+        <title>início | ig.news</title>
+      </Head>
+      <h1 className={styles.title}>
+        Hello <span>come</span>
+      </h1>
+    </>
   )
 }
